f55d29cb68279d578dc0199ac0b927c72685c9ed
iperdids <ipersids@student.hive.fi>
2024-11-11 19:53:40 +0200

    Add header file ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 19:02:08 by ipersids          #+#    #+#             */
+/*   Updated: 2024/11/11 19:28:17 by ipersids         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>				// va_list, va_start, and va_end.
+
+int	ft_printf(const char *, ...);
+
+#endif
