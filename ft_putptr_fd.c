18b18abf1810d74133f98c088047816a6173eaa3
iperdids <ipersids@student.hive.fi>
2024-11-13 18:46:38 +0200

    Add ft_putptr_fd.c

---
+++ ft_putptr_fd.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 18:35:41 by ipersids          #+#    #+#             */
+/*   Updated: 2024/11/13 18:43:29 by ipersids         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_putptr_fd(unsigned int n, int fd)
+{
+	size_t	len;
+
+	if (fd < 0)
+		return (0);
+	len = 0;
+	len += write(fd, "0x", 2);
+	len += ft_puthex_fd(n, fd, 0);
+	return (len);
+}
