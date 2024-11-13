2b5b124e4d2ea2f3f0aa7160ad992a1792691b35
iperdids <ipersids@student.hive.fi>
2024-11-13 18:49:28 +0200

    Update ft_printf.c and ft_putptr_fd.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,14 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 23:33:28 by ipersids          #+#    #+#             */
-/*   Updated: 2024/11/13 01:03:13 by ipersids         ###   ########.fr       */
+/*   Updated: 2024/11/13 18:52:27 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static short int	format_is_valid(const char *format);
+static const char	*print_content(va_list args, const char *f, size_t *len);
 
 /**
  * @brief Print formatted data to stdout.
@@ -41,19 +44,55 @@
  */
 int	ft_printf(const char *format, ...)
 {
-	va_list args;
-	int		len;
+	va_list	args;
+	size_t	len;
 
 	len = 0;
 	va_start(args, format);
 	while (*format != '\0')
 	{
+		if (*format == '%' && format_is_valid(format + 1) != 0)
+		{
+			format = print_content(args, format + 1, &len);
+			continue ;
+		}
+		write(1, format, 1);
+		format++;
+		len++;
+	}
+	va_end(args);
+	return (len);
+}
+
 		// %[flags][width][.precision][length]specifier
 		// 1. Search for %
-		// 2. Check flags #, (space) and +
-		// 3. Check width ???
 		// 4. Check specifier
 		// 5. Print content or return -1 if error
-	}
-	return (len);
+
+static short int	format_is_valid(const char *format)
+{
+	if (ft_strchr(SPECIFIERS, *format) != 0)
+		return (1);
+	return (0);
+}
+
+static const char	*print_content(va_list args, const char *f, size_t *len)
+{
+	if (*f == 'c')
+		*len += ft_putchar_fd(va_arg(args, int), 1);
+	else if (*f == 's')
+		*len += ft_putstr_fd(va_arg(args, char *), 1);
+	if (*f == 'd' || *f == 'i')
+		*len += ft_putnbr_fd(va_arg(args, int), 1);
+	if (*f == 'u')
+		*len += ft_putunbr_fd(va_arg(args, unsigned int), 1);
+	if (*f == 'x' || *f == 'X')
+		*len += ft_puthex_fd(va_arg(args, int), 1, *f == 'X');
+	if (*f == 'p')
+		*len += ft_putptr_fd(va_arg(args, unsigned int), 1);
+	else if (*f == '%' || *f == '\0')
+		*len += ft_putchar_fd('%', 1);
+	if (*f == '\0')
+		return (f);
+	return (f + 1);
 }
