4157dd2a929b11ab9be5ebf04ca05e16e0f33798
iperdids <ipersids@student.hive.fi>
2024-11-14 13:20:01 +0200

    Fix ft_putptr_fd.c

--- ft_putptr_fd.c
+++ ft_putptr_fd.c
@@ -6,20 +6,27 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:35:41 by ipersids          #+#    #+#             */
-/*   Updated: 2024/11/13 18:52:45 by ipersids         ###   ########.fr       */
+/*   Updated: 2024/11/14 13:15:25 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-size_t	ft_putptr_fd(unsigned int n, int fd)
+size_t	ft_putptr_fd(unsigned long long n, int fd)
 {
 	size_t	len;
+	char	*base;
 
 	if (fd < 0)
 		return (0);
+	if (!n)
+		return (write(1, "(nil)", 5));
+	base = "0123456789abcdef";
 	len = 0;
-	len += write(fd, "0x", 2);
-	len += ft_puthex_fd(n, fd, 0);
+	if (n > 15)
+		len += ft_putptr_fd(n / 16, fd);
+	if (len == 0)
+		len += write(fd, "0x", 2);
+	len += write(fd, &base[n % 16], 1);
 	return (len);
 }
