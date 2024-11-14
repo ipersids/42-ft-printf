1197ea8a963cd9593fec8abac9d888cd85e8f431
Julia Llama <julia.persidskaia@gmail.com>
2024-11-14 10:19:30 +0200

    Fix ft_printf.c
    Fix the output for multiple args.

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,14 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 23:33:28 by ipersids          #+#    #+#             */
-/*   Updated: 2024/11/13 18:52:27 by ipersids         ###   ########.fr       */
+/*   Updated: 2024/11/14 10:16:18 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 static short int	format_is_valid(const char *format);
-static const char	*print_content(va_list args, const char *f, size_t *len);
+static const char	*print_content(va_list *args, const char *f, size_t *len);
 
 /**
  * @brief Print formatted data to stdout.
@@ -53,7 +53,7 @@ int	ft_printf(const char *format, ...)
 	{
 		if (*format == '%' && format_is_valid(format + 1) != 0)
 		{
-			format = print_content(args, format + 1, &len);
+			format = print_content(&args, format + 1, &len);
 			continue ;
 		}
 		write(1, format, 1);
@@ -76,20 +76,20 @@ static short int	format_is_valid(const char *format)
 	return (0);
 }
 
-static const char	*print_content(va_list args, const char *f, size_t *len)
+static const char	*print_content(va_list *args, const char *f, size_t *len)
 {
 	if (*f == 'c')
-		*len += ft_putchar_fd(va_arg(args, int), 1);
+		*len += ft_putchar_fd(va_arg(*args, int), 1);
 	else if (*f == 's')
-		*len += ft_putstr_fd(va_arg(args, char *), 1);
+		*len += ft_putstr_fd(va_arg(*args, char *), 1);
 	if (*f == 'd' || *f == 'i')
-		*len += ft_putnbr_fd(va_arg(args, int), 1);
+		*len += ft_putnbr_fd(va_arg(*args, int), 1);
 	if (*f == 'u')
-		*len += ft_putunbr_fd(va_arg(args, unsigned int), 1);
+		*len += ft_putunbr_fd(va_arg(*args, unsigned int), 1);
 	if (*f == 'x' || *f == 'X')
-		*len += ft_puthex_fd(va_arg(args, int), 1, *f == 'X');
+		*len += ft_puthex_fd(va_arg(*args, int), 1, *f == 'X');
 	if (*f == 'p')
-		*len += ft_putptr_fd(va_arg(args, unsigned int), 1);
+		*len += ft_putptr_fd(va_arg(*args, unsigned int), 1);
 	else if (*f == '%' || *f == '\0')
 		*len += ft_putchar_fd('%', 1);
 	if (*f == '\0')
